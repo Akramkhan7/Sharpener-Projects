685fa4c137c17497669ee6c4de7a52daae4cdb53
Akramkhan7 <m.akramkhan6690@gmail.com>
2026-03-31 15:23:40 +0530

    DOne

--- Todo/src/Todo.jsx
+++ Todo/src/Todo.jsx
@@ -3,8 +3,17 @@ import React, { use, useEffect, useState } from "react";
 const Todo = () => {
   const [task, setTask] = useState("");
   const [editId, setEditId] = useState(null);
-  const [search, setSearch] = useState('');
-  const [categoryFilter,setCategoryFilter] = useState('All Categories')
+  const [search, setSearch] = useState("");
+  const [statusFilter, setStatusFilter] = useState("All");
+  const [categoryFilter, setCategoryFilter] = useState("All Categories");
+
+  const [taskDelete, setTaskDelete] = useState(null);
+  const [showUndo, setShowUndo] = useState(false);
+  const [timerId, setTimerId] = useState(null);
+
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 5;
+
   const [tasks, setTasks] = useState(() => {
     try {
       const data = JSON.parse(localStorage.getItem("tasks")) || [];
@@ -18,33 +27,41 @@ const Todo = () => {
 
   let finalTask = tasks;
 
-  if(search){
-    finalTask =  finalTask.filter((t)=>
-       t.title.toLowerCase().includes(search.toLowerCase())
-    )
+
+  if (search) {
+    finalTask = finalTask.filter((t) =>
+      t.title.toLowerCase().includes(search.toLowerCase()),
+    );
   }
 
-  if(categoryFilter !== 'All Categories'){
-    finalTask = finalTask.filter((t)=>
-      t.category === categoryFilter
-    )
+  if (categoryFilter !== "All Categories") {
+    finalTask = finalTask.filter((t) => t.category === categoryFilter);
   }
+
+  if (statusFilter !== "All") {
+    finalTask = finalTask.filter((t) => {
+      if (statusFilter === "Completed") return t.completed === true;
+      if (statusFilter === "Pending") return t.completed === false;
+    });
+  }
+
+    const totalPages = Math.ceil(finalTask.length / itemsPerPage);
+  const startIndex = (currentPage - 1) * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
+  const paginatedTasks = finalTask.slice(startIndex,endIndex);
+
 
   const handleTask = () => {
     if (task.trim() === "") return;
 
     if (editId !== null) {
-
       const update = tasks.map((t) =>
-        t.id === editId ? { ...t, title: task,category,priority } : t,
+        t.id === editId ? { ...t, title: task, category, priority } : t,
       );
       setTasks(update);
       setEditId("");
       setTask("");
-
-    } 
-    else
-       {
+    } else {
       const newTask = {
         id: Date.now(),
         title: task,
@@ -54,14 +71,40 @@ const Todo = () => {
       };
       setTasks([...tasks, newTask]);
       setTask("");
-
     }
   };
 
   const deleteTask = (id) => {
+    const taskToDelete = tasks.find((t) => t.id === id);
+
+    if (timerId) {
+      clearTimeout(timerId);
+    }
+    setTaskDelete(taskToDelete);
+
     const update = tasks.filter((t) => t.id !== id);
-    console.log(update);
     setTasks(update);
+
+    setShowUndo(true);
+
+    const newTimer = setTimeout(() => {
+      setShowUndo(false);
+      setTaskDelete(null);
+    }, 5000);
+
+    setTimerId(newTimer);
+  };
+
+  const handleUndo = () => {
+    if (!taskDelete) return;
+
+    setTasks((prev) => [...prev, taskDelete]);
+    setTaskDelete(null);
+    setShowUndo(false);
+
+    if (timerId) {
+      clearTimeout(timerId);
+    }
   };
 
   const toggleComplete = (id) => {
@@ -75,15 +118,19 @@ const Todo = () => {
     setTasks(updatedData);
   };
 
+  const filteredTasks = tasks.filter((t) =>
+    t.title.toLowerCase().includes(search.toLowerCase()),
+  );
 
-const filteredTasks = tasks.filter((t) =>
-  t.title.toLowerCase().includes(search.toLowerCase())
-);
+  const categoryFilteredTasks = tasks.filter((t) => {
+    if (categoryFilter === "All Categories") return true;
+    return t.category === categoryFilter;
+  });
 
-const categoryFilteredTasks = tasks.filter((t) => {
-  if (categoryFilter === "All Categories") return true;
-  return t.category === categoryFilter;
-});
+  const statusFilterTasks = tasks.filter((t) => {
+    if (statusFilter == "All") return true;
+    return t.status === statusFilter;
+  });
 
   useEffect(() => {
     localStorage.setItem("tasks", JSON.stringify(tasks));
@@ -93,7 +140,6 @@ const categoryFilteredTasks = tasks.filter((t) => {
     <div className="min-h-screen w-full bg-gray-100 flex flex-col items-center p-6">
       {/* Header */}
       <h1 className="text-3xl font-bold mb-6">Todo App</h1>
-
       {/* Top Controls */}
       <div className="w-full max-w-4xl bg-white p-4 rounded-xl shadow space-y-4">
         {/* Add Task */}
@@ -139,23 +185,28 @@ const categoryFilteredTasks = tasks.filter((t) => {
           {/* Search */}
           <input
             type="text"
-            onChange={(e)=>setSearch(e.target.value)}
+            onChange={(e) => setSearch(e.target.value)}
             placeholder="Search tasks..."
             className="px-4 py-2 border rounded-lg w-full md:w-1/3"
           />
 
           {/* Filters */}
           <div className="flex gap-2 flex-wrap">
-            <select className="px-3 py-2 border rounded-lg" 
+            <select
+              className="px-3 py-2 border rounded-lg"
+              value={statusFilter}
+              onChange={(e) => setStatusFilter(e.target.value)}
             >
               <option>All</option>
               <option>Completed</option>
               <option>Pending</option>
             </select>
 
-            <select className="px-3 py-2 border rounded-lg"
-            value={categoryFilter}
-            onChange={(e)=>setCategoryFilter(e.target.value)}>
+            <select
+              className="px-3 py-2 border rounded-lg"
+              value={categoryFilter}
+              onChange={(e) => setCategoryFilter(e.target.value)}
+            >
               <option>All Categories</option>
               <option>Work</option>
               <option>Personal</option>
@@ -164,86 +215,104 @@ const categoryFilteredTasks = tasks.filter((t) => {
           </div>
         </div>
       </div>
-
       {/* Undo Banner */}
-      <div className="mt-4 w-full max-w-4xl bg-yellow-100 text-yellow-800 px-4 py-2 rounded-lg flex justify-between items-center">
-        <span>Task deleted</span>
-        <button className="font-semibold underline">Undo</button>
-      </div>
-
+      {showUndo && taskDelete && (
+        <div className="mt-4 w-full max-w-4xl bg-yellow-100 text-yellow-800 px-4 py-2 rounded-lg flex justify-between items-center">
+          <span>Task deleted</span>
+          <button className="font-semibold underline" onClick={handleUndo}>
+            Undo
+          </button>
+        </div>
+      )}
       {/* Task List */}
       <div className="mt-6 w-full max-w-4xl space-y-3">
-       {finalTask.length === 0 ?  <h2>No matched found!</h2> : 
-        finalTask.map((t) => (
-          <div
-            key={t.id}
-            className="bg-white p-4 rounded-lg shadow flex justify-between items-center"
-          >
-            {/* Left */}
-            <div className="flex items-center gap-3">
-              <input
-                type="checkbox"
-                checked={t.completed}
-                onChange={() => toggleComplete(t.id)}
-              />
+        {finalTask.length === 0 ? (
+          <h2>No matched found!</h2>
+        ) : (
+          paginatedTasks.map((t) => (
+            <div
+              key={t.id}
+              className="bg-white p-4 rounded-lg shadow flex justify-between items-center"
+            >
+              {/* Left */}
+              <div className="flex items-center gap-3">
+                <input
+                  type="checkbox"
+                  checked={t.completed}
+                  onChange={() => toggleComplete(t.id)}
+                />
 
-              <div>
-                <p className={t.completed ? "line-through" : "font-medium"}>
-                  {t.title}
-                </p>
-                <div className="text-sm text-gray-500 flex gap-2">
-                  <span>{t.category}</span>
-                  <span>•</span>
-                  <span className="text-red-500">{t.priority}</span>
+                <div>
+                  <p className={t.completed ? "line-through" : "font-medium"}>
+                    {t.title}
+                  </p>
+                  <div className="text-sm text-gray-500 flex gap-2">
+                    <span>{t.category}</span>
+                    <span>•</span>
+                    <span className="text-red-500">{t.priority}</span>
+                  </div>
                 </div>
               </div>
+
+              {/* Right Actions */}
+              <div className="flex gap-2">
+                <button
+                  className="px-3 py-1 border rounded"
+                  onClick={() => {
+                    setEditId(t.id);
+                    setTask(t.title);
+                    setCategory(t.category);
+                    setPriority(t.priority);
+                  }}
+                >
+                  Edit
+                </button>
+                <button
+                  className="px-3 py-1 border rounded text-red-500"
+                  onClick={() => deleteTask(t.id)}
+                >
+                  Delete
+                </button>
+              </div>
             </div>
-
-            {/* Right Actions */}
-            <div className="flex gap-2">
-              <button
-                className="px-3 py-1 border rounded"
-                onClick={() => {
-                  setEditId(t.id);
-                  setTask(t.title)
-                  setCategory(t.category);
-                  setPriority(t.priority);
-                }}
-              >
-                Edit
-              </button>
-              <button
-                className="px-3 py-1 border rounded text-red-500"
-                onClick={() => deleteTask(t.id)}
-              >
-                Delete
-              </button>
-            </div>
-          </div>
-        ))
-       }
-         
-       
-
-        
-       
-
+          ))
+        )}
       </div>
-
       {/* Pagination */}
-      <div className="mt-6 flex gap-2 items-center">
-        <button className="px-3 py-1 border rounded disabled:opacity-50">
-          Previous
-        </button>
+     <div className="mt-6 flex gap-2 items-center">
 
-        <button className="px-3 py-1 border rounded bg-black text-white">
-          1
-        </button>
-        <button className="px-3 py-1 border rounded">2</button>
-        <button className="px-3 py-1 border rounded">3</button>
+  {/* Prev */}
+  <button
+    onClick={() => setCurrentPage((p) => p - 1)}
+    disabled={currentPage === 1}
+    className="px-3 py-1 border rounded disabled:opacity-50"
+  >
+    Previous
+  </button>
 
-        <button className="px-3 py-1 border rounded">Next</button>
-      </div>
+  {/* Page Numbers */}
+  {[...Array(totalPages)].map((_, i) => (
+    <button
+      key={i}
+      onClick={() => setCurrentPage(i + 1)}
+      className={`px-3 py-1 border rounded ${
+        currentPage === i + 1 ? "bg-black text-white" : ""
+      }`}
+    >
+      {i + 1}
+    </button>
+  ))}
+
+  {/* Next */}
+  <button
+    onClick={() => setCurrentPage((p) => p + 1)}
+    disabled={currentPage === totalPages}
+    className="px-3 py-1 border rounded disabled:opacity-50"
+  >
+    Next
+  </button>
+
+</div>
     </div>
   );
 };
