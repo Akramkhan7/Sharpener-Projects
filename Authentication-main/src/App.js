ea49af54a1e2922917d22fc601c5ab457172fea1
Akramkhan7 <m.akramkhan6690@gmail.com>
2026-07-09 13:37:54 +0530

    DOne

--- Authentication-main/src/App.js
+++ Authentication-main/src/App.js
@@ -1,39 +1,56 @@
-import { Switch, Route,Redirect } from "react-router-dom";
+import { Switch, Route, Redirect } from "react-router-dom";
 
 import Layout from "./components/Layout/Layout";
 import UserProfile from "./components/Profile/UserProfile";
 import AuthPage from "./pages/AuthPage";
 import HomePage from "./pages/HomePage";
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import AuthContext from "./components/Store/AuthContext";
 
 function App() {
   const authCtx = useContext(AuthContext);
- return (
-  <Layout>
-    <Switch>
-      <Route path="/" exact>
-        <HomePage />
-      </Route>
+  const API_KEY = process.env.REACT_APP_FIREBASE_API_KEY;
+  useEffect(() => {
+    if (!authCtx.token) return;
+    try {
+      const checkToken = async () => {
+        const res = await fetch(
+          `https://identitytoolkit.googleapis.com/v1/accounts:lookup?key=${API_KEY}`,
+          {
+            method: "POST",
+            body: JSON.stringify({
+              idToken: authCtx.token,
+            }),
+            headers: {
+              "Content-Type": "application/json",
+            },
+          },
+        );
+        if (!res.ok) {
+          authCtx.logout();
+        }
+      };
+    } catch (err) {
+      authCtx.logout();
+    }
+  }, [authCtx]);
+  return (
+    <Layout>
+      <Switch>
+        <Route path="/" exact>
+          <HomePage />
+        </Route>
 
-      <Route path="/auth">
-        {!authCtx.isLoggedIn ? (
-          <AuthPage />
-        ) : (
-          <Redirect to="/profile" />
-        )}
-      </Route>
+        <Route path="/auth">
+          {!authCtx.isLoggedIn ? <AuthPage /> : <Redirect to="/profile" />}
+        </Route>
 
-      <Route path="/profile">
-        {authCtx.isLoggedIn ? (
-          <UserProfile />
-        ) : (
-          <Redirect to="/auth" />
-        )}
-      </Route>
-    </Switch>
-  </Layout>
-);
+        <Route path="/profile">
+          {authCtx.isLoggedIn ? <UserProfile /> : <Redirect to="/auth" />}
+        </Route>
+      </Switch>
+    </Layout>
+  );
 }
 
 export default App;
